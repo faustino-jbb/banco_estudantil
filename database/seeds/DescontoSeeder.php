14a4f7a0a2f1ff50fc4b42a39bd2c8e213063eba
faustino-jbb <nic340k@gmail.com>
2021-04-18 09:36:14 +0100

    criando seeders do sistema

---
+++ database/seeds/DescontoSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DescontoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
